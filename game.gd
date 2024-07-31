441b0b4982928f008f768724cd222ed353cd34ff
jdbecker <jdbecker20@gmail.com>
2024-07-31 12:45:15 -0500

    unused reference

--- game.gd
+++ game.gd
@@ -1,7 +1,6 @@
 extends Node
 
 @onready var level_switcher: LevelSwitcher = $LevelSwitcher
-const TEST_WORLD = preload("res://worlds/test_world.tscn")
 
 func _on_start_menu_hosted(path: String) -> void:
 	level_switcher.change_level.call_deferred(path)
